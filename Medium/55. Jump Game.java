eea7f4021c71078e5e80290635df7b0957b1ed9b
Arjun Dey <arjun.dey.2612@gmail.com>
2023-04-30 22:24:10 +0530

    first commit

---
+++ Medium/55. Jump Game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       int n = nums.length ;
+       int lastpos = n - 1 ;
+
+       for( int i = n - 1 ; i >= 0 ; i--){
+           if( i + nums[i] >= lastpos){
+                lastpos = i;
+           }   
+       }
+       return lastpos == 0;
+    }
+}
